37d322da7cd3c1cec7e3beb66689f26e8be04434
Shanan <Mohamed.shanan48@gmail.com>
2019-04-16 02:48:11 +0200

    Editing Some code

--- Course1_Algorithmic_Toolbox/Week_3/dot_product/dot_product.cpp
+++ Course1_Algorithmic_Toolbox/Week_3/dot_product/dot_product.cpp
@@ -1,14 +1,18 @@
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+
+
 #include <vector>
 
 using std::vector;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
+  std::sort(a.begin(), a.end());
+  std::sort(b.begin(), b.end());
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
+    result += ((long long)a[i]) * b[i];
   }
   return result;
 }
@@ -24,4 +28,5 @@ int main() {
     std::cin >> b[i];
   }
   std::cout << max_dot_product(a, b) << std::endl;
+  return 0;
 }
